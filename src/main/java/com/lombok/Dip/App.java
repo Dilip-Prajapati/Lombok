ba3b64c59e2b44705b8703378c4d0d153f1cba35
Dilip-Prajapati <79246741+Dilip-Prajapati@users.noreply.github.com>
2021-03-22 15:55:55 +0530

    Lombok

---
+++ src/main/java/com/lombok/Dip/App.java
@@ -0,0 +1,19 @@
+package com.lombok.Dip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        emp e = new emp();
+//        e.setName("Dipu");
+//        e.setLastName("Prajapati");
+//        System.out.println(e.getName());
+        System.out.println(e);
+    }
+
+}
---
+++ src/main/java/com/lombok/Dip/emp.java
@@ -0,0 +1,11 @@
+package com.lombok.Dip;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+public class emp {
+
+	private String name;
+	private String lastName;
+}
